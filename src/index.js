8055e794a38d24406fb80f613b2a05493fa04db1
DinaTaklit <ez_taklit@esi.dz>
2020-04-27 20:45:41 +0100

    feat: Create App Entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import {render} from "react-dom";
+import HomePage from './components/HomePage';
+
+render(
+    <HomePage></HomePage>,
+    document.getElementById('root')
+);
