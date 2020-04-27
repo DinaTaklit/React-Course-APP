55dcd0b936f1034d991a55a430d7717ca7a5f5ca
DinaTaklit <ez_taklit@esi.dz>
2020-04-27 20:31:51 +0100

    feat: create out first componenet that will display our home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// this componenet will display our home page
+function HomePage() {
+  return (
+  <div className="jumbotron">
+    <h1>Pluralsight Administration</h1>
+    <p> React, Flux, and React Router for ultra-responsive web apps. </p>
+  </div>
+  );
+}
+
+export default HomePage;
