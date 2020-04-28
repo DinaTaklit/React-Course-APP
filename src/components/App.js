ff50abb07259d51d583fe9baf3bb757bbdb32426
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 11:39:15 +0100

    feat: app.js Declaring Routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from './CoursesPage';
+import { Route } from 'react-router-dom';
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage></CoursesPage>;
-    if (route === "/about") return <AboutPage></AboutPage>;
-    return <HomePage></HomePage>;
-  }
   return (
     <div className="container-fluid">
       <Header></Header>
-      {getPage()} {/*to call the function*/}
+      <Route path="/" exact component={HomePage}/>
+      <Route path="/courses" exact component={CoursesPage}/>
+      <Route path="/about" exact component={AboutPage}/>
     </div>
   );
 }
