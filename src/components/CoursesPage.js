949d3d21767cf7e5ad58fa312b9d9ffdf9073072
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 10:03:42 +0100

    refact: Use function istead of calass for courses components
        - use state hooks to manipulate states
        - yse side effects to get courses instead of the previous method did mount

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursePage extends React.Component {
-  state = {
-    courses: [],
-  };
-  componentDidMount() {
-    getCourses().then((courses) => this.setState({ courses: courses }));
-  }
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                <tr key={course.id}>
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+function CoursePage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  });
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursePage;
