d5aa6f04c008f5ef041912eed1692dfc80161b3a
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 12:44:32 +0100

    update: App=> Add route for MangeCourse Page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Header from "./common/Header";
 import CoursesPage from './CoursesPage';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from "./PageNotFound";
-
+import ManageCoursePage from './ManageCoursePage';
 function App() {
   return (
     <div className="container-fluid">
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={HomePage}/>
         <Route path="/courses" exact component={CoursesPage}/>
         <Route path="/about" exact component={AboutPage}/>
+        <Route path="/course" component={ManageCoursePage}></Route>
         <Redirect from="/about-page" to="about"></Redirect>
         <Route component={NotFoundPage}/> 
       </Switch>
