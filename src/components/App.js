9be26b22154a79c30d6480d28c7fb5cbbcac7ec5
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 12:21:26 +0100

    feat: App=> Use redirect url from an old link to new one

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from './CoursesPage';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from "./PageNotFound";
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={HomePage}/>
         <Route path="/courses" exact component={CoursesPage}/>
         <Route path="/about" exact component={AboutPage}/>
+        <Redirect from="/about-page" to="about"></Redirect>
         <Route component={NotFoundPage}/> 
       </Switch>
     </div>
