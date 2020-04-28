90ec4c50fbd0dc15fd941af6ab798e7c5f30d777
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 11:47:15 +0100

    feat: HomePage=> Use Links compoents to navigate to the about page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 // this componenet will display our home page
 function HomePage() {
   return (
   <div className="jumbotron">
     <h1>Pluralsight Administration</h1>
     <p> React, Flux, and React Router for ultra-responsive web apps. </p>
-    <a href="/about">About</a>
+    <Link to="/about" className="btn btn-primary">About</Link>
   </div>
   );
 }
