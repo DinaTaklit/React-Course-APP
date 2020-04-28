d20cf8dfe546e1077f1446d7cde2b1a88d2aeb57
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 12:02:14 +0100

    fix: Header=> Add exact to to the home link to avoind the alwyas matching

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ function Header() {
   const activeStyle = {color: "orange"};
   return (
     <nav>
-      <NavLink activeStyle={activeStyle} to="/">Home
+      <NavLink activeStyle={activeStyle} exact to="/">Home
       </NavLink>
       {" | "} 
       <NavLink activeStyle={activeStyle} to="/courses">Courses
