4b1f60f547672ff40e4b9c0f4e54a1447da1b128
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 15:57:06 +0100

    update: reference CourseForm from ManageCoursePage

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => {
   return (
     <>
       <h2> Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug} {/* TO show the slug placeholder in the link*/}
+      <CourseForm></CourseForm>
     </>
   );
 };
