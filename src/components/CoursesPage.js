a45b5697aef33d39255ae2e0c7d527f6b3de7549
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 09:18:32 +0100

    feat: Display Array in a Table via Map in course component

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -9,7 +9,31 @@ class CoursePage extends React.Component {
     getCourses().then((courses) => this.setState({ courses: courses }));
   }
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+    </>
+    );
   }
 }
 
