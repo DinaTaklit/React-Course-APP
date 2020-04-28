1019e0e3caeff0dc982d10916fd79123c7f47093
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 12:13:43 +0100

    feat: App=> Add route for page not found     - Use switch to avoid redring the page not found with each above route since it has no specified path

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from './CoursesPage';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import NotFoundPage from "./PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header></Header>
-      <Route path="/" exact component={HomePage}/>
-      <Route path="/courses" exact component={CoursesPage}/>
-      <Route path="/about" exact component={AboutPage}/>
+      <Switch>
+        <Route path="/" exact component={HomePage}/>
+        <Route path="/courses" exact component={CoursesPage}/>
+        <Route path="/about" exact component={AboutPage}/>
+        <Route component={NotFoundPage}/> 
+      </Switch>
     </div>
   );
 }
