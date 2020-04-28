1816fa7930f483ddbd01b69a4ef43e2fb5b7ce66
DinaTaklit <ez_taklit@esi.dz>
2020-04-28 13:07:31 +0100

    feat: ManageCoursePage=> use prompot on transition

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = (props) => {
-  debugger;
   return (
     <>
       <h2> Manage Course</h2>
+      <Prompt when={true} message="Are you sure you want to leave?" />
       {props.match.params.slug} {/* TO show the slug placeholder in the link*/}
     </>
   );
