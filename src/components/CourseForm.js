2fbef7a2bd6171bcd81bea6b23e786c129aee46a
DinaTaklit <ez_taklit@esi.dz>
2020-04-29 11:20:50 +0100

    update: CourseForm=> Consume the reusable input

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,21 +1,16 @@
 import React from "react";
+import TextInput from "./common/TextInput";
 
 function CourseForm(props) {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
-            id="title"
-            type="text"
-            onChange={props.onChange}
-            name="title"
-            className="form-control"
-            value={props.course.title}
-          />
-        </div>
-      </div>
+        <TextInput
+        id="title"
+        label="Title"
+        onChange={props.onChange}
+        name="title"
+        value={props.course.title}
+      />
 
       <div className="form-group">
         <label htmlFor="author">Author</label>
@@ -34,19 +29,13 @@ function CourseForm(props) {
         </div>
       </div>
 
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <div className="field">
-          <input
-            type="text"
-            id="category"
-            name="category"
-            onChange={props.onChange}
-            className="form-control"
-            value={props.course.category}
-          />
-        </div>
-      </div>
+      <TextInput
+        label="Category"
+        id="category"
+        name="category"
+        onChange={props.onChange}
+        value={props.course.category}
+      />
 
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
