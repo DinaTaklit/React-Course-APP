c927e32b9c898210b7cc3fd5dfcc59f6812bed2f
DinaTaklit <ez_taklit@esi.dz>
2020-04-29 15:45:39 +0100

    update: Change the className of the app to container instead of container-fluid

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <ToastContainer autoClose={3000} hideProgressBar/>
       <Header/>
       <Switch>
