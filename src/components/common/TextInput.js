1a4da1e0d7694de9f1e8d761b1673a60556d5dd0
DinaTaklit <ez_taklit@esi.dz>
2020-04-29 10:45:56 +0100

    feat: TextInput=> Add input validator     - It serves for dev it gives an idea about the vars that exists

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TextInput(props) {
-   // to handle the error once we need to add error wrapper as boostrap logic
-   let wrapperClass = "form-group";
-   if (props.error && props.error.length > 0){
-       wrapperClass += "has-error";
-   }
+  // to handle the error once we need to add error wrapper as boostrap logic
+  let wrapperClass = "form-group";
+  if (props.error.length > 0) {
+    wrapperClass += "has-error";
+  }
   return (
     <div className={wrapperClass}>
       <label htmlFor={props.id}>{props.label}</label>
@@ -19,10 +20,23 @@ function TextInput(props) {
           value={props.value}
         />
       </div>
-    {/* We use the and operator to execute the second item if the first one is correct*/}
-  {props.error && <div className="alert alert-danger">{props.error}</div>}
+      {/* We use the and operator to execute the second item if the first one is correct*/}
+      {props.error && <div className="alert alert-danger">{props.error}</div>}
     </div>
   );
 }
+
+TextInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  error: PropTypes.string,
+};
+// add default prop for the error
+TextInput.defaultProps = {
+  error: "", // by adding this we do not need to check aboce the existance of the error coz it already exists ;)
+};
 
 export default TextInput;
