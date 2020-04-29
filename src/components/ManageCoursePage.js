c5aab9dc9e6938fbde1d3a1737a8be2fbcfce12b
DinaTaklit <ez_taklit@esi.dz>
2020-04-29 23:40:49 +0100

    feat: CourseForm=> Add PropTypes to document a component's expectations

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
-import * as coureApi from '../api/courseApi'; // import all the function in the course Apo
-
+import * as coureApi from "../api/courseApi"; // import all the function in the course Apo
 import { toast } from "react-toastify";
+import PropTypes from "prop-types";
 
 const ManageCoursePage = (props) => {
   const [errors, setErrors] = useState({});
@@ -11,9 +11,9 @@ const ManageCoursePage = (props) => {
     slug: "",
     title: "",
     authorId: null,
-    category: ""
+    category: "",
   });
-  function handleChange({target}) {
+  function handleChange({ target }) {
     /*this distructring is like doing const target = event.target*/
 
     /* Use computed property to set the values to inputs by their name to avoind create a hndler for each one */
@@ -22,22 +22,22 @@ const ManageCoursePage = (props) => {
       [target.name]: target.value,
     });
   }
-  function formIsValid(){
-    const _errors ={};
-    if(!course.title) _errors.title = "Title is required";
-    if(!course.authorId) _errors.authorId = "AuthorId is required";
-    if(!course.category) _errors.category = "Category is required";
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "AuthorId is required";
+    if (!course.category) _errors.category = "Category is required";
     setErrors(_errors);
-    // Form is valid if the errors object has no properties 
+    // Form is valid if the errors object has no properties
     return Object.keys(_errors).length === 0;
   }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    if(!formIsValid()) return;
-    // since saveCourse return a promise we can add code :D 
-    coureApi.saveCourse(course).then(()=>{
-      // Since this compoenet is loaded via React Router's route componet we have access to React Router's history object on props so we can programmatically redirect the user here after the save is completed 
+    if (!formIsValid()) return;
+    // since saveCourse return a promise we can add code :D
+    coureApi.saveCourse(course).then(() => {
+      // Since this compoenet is loaded via React Router's route componet we have access to React Router's history object on props so we can programmatically redirect the user here after the save is completed
       props.history.push("/courses");
       toast.success(`The course: ${course.title} was saved successfuly!`);
     });
@@ -45,14 +45,20 @@ const ManageCoursePage = (props) => {
   return (
     <>
       <h2> Manage Course</h2>
-      <CourseForm 
-      errors={errors}
-      course={course} 
-      onChange={handleChange}
-      onSubmit={handleSubmit}
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
       />
     </>
   );
 };
 
+CourseForm.prototype = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+};
 export default ManageCoursePage;
