3e564ab34a0c06ac971b60bfff57ed7feafbb16a
DinaTaklit <ez_taklit@esi.dz>
2020-04-29 10:38:53 +0100

    feat: TextInput=> add error class to display in case of error

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 function TextInput(props) {
+   // to handle the error once we need to add error wrapper as boostrap logic
+   let wrapperClass = "form-group";
+   if (props.error && props.error.length > 0){
+       wrapperClass += "has-error";
+   }
   return (
-    <div className="form-group">
+    <div className={wrapperClass}>
       <label htmlFor={props.id}>{props.label}</label>
       <div className="field">
         <input
@@ -14,6 +19,8 @@ function TextInput(props) {
           value={props.value}
         />
       </div>
+    {/* We use the and operator to execute the second item if the first one is correct*/}
+  {props.error && <div className="alert alert-danger">{props.error}</div>}
     </div>
   );
 }
