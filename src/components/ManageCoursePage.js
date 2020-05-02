36c7a0ab2c9712891c27647f0eb1ad812c61cb17
DinaTaklit <ez_taklit@esi.dz>
2020-05-02 09:04:26 +0100

    commit ManageCoursePage=> Populate form the URL to edit an existing course

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
-import * as coureApi from "../api/courseApi"; // import all the function in the course Apo
+import * as courseApi from "../api/courseApi"; // import all the function in the course Apo
 import { toast } from "react-toastify";
 import PropTypes from "prop-types";
 
@@ -13,9 +13,20 @@ const ManageCoursePage = (props) => {
     authorId: null,
     category: "",
   });
+
+  // Get the course once the componenet mount
+  useEffect(() => {
+    console.log("Use effect");
+    const slug = props.match.params.slug; // get the slug from /courses/:slug
+    console.log(slug);
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    }
+    // Add the array of dependency to keep an eye on the slug, when the slug run the effect should rerun
+  }, [props.match.params.slug]);
+
   function handleChange({ target }) {
     /*this distructring is like doing const target = event.target*/
-
     /* Use computed property to set the values to inputs by their name to avoind create a hndler for each one */
     setCourse({
       ...course,
@@ -36,7 +47,7 @@ const ManageCoursePage = (props) => {
     event.preventDefault();
     if (!formIsValid()) return;
     // since saveCourse return a promise we can add code :D
-    coureApi.saveCourse(course).then(() => {
+    courseApi.saveCourse(course).then(() => {
       // Since this compoenet is loaded via React Router's route componet we have access to React Router's history object on props so we can programmatically redirect the user here after the save is completed
       props.history.push("/courses");
       toast.success(`The course: ${course.title} was saved successfuly!`);
